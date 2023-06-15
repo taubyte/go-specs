53df337267695577eb0795daa421d0949bca4e48
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-06-15 18:28:24 -0500

    fix interfaces

--- common/vars.go
+++ common/vars.go
@@ -15,3 +15,6 @@ const (
 
 // TODO remove this and iterate, default branch should be gathered from a given repository
 var DefaultBranch = "master"
+
+var Protocols = []string{"auth", "patrick", "monkey", "tns", "hoarder", "node", "seer"}
+var HttpProtocols = []string{"patrick", "node", "seer", "auth"}
