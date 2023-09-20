4d29b0a97a314005a049e125c4c39f24270282e1
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-09-19 19:04:06 -0500

    bring back funky WhiteListedDomains

--- domain/vars.go
+++ domain/vars.go
@@ -8,4 +8,8 @@ import (
 
 const PathVariable common.PathVariable = "domains"
 
+var WhiteListedDomains = []string{"nodes.taubyte.com", "nodes.taubyte.network"}
+
+var TaubyteServiceDomain = regexp.MustCompile(`^[^.]+\.taubyte\.com$`)
 var SpecialDomain = regexp.MustCompile(`^[^.]+\.g\.tau\.link$`)
+var TaubyteHooksDomain = regexp.MustCompile(`hooks.git.taubyte.com`)
